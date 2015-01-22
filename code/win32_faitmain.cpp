98e22f21dbb72515a278b032e2d11008eb41f429
fgd99 <fabriceguydominguez@gmail.com>
2015-01-22 07:34:47 +0100

    Début de gestion de la fenêtre principale
    juste le code pour débuter l'ouverture de la fenêtre principale

--- code/win32_faitmain.cpp
+++ code/win32_faitmain.cpp
@@ -1,15 +1,61 @@
 #include <Windows.h>
 
+LRESULT CALLBACK MainWindowCallback(
+  HWND Window,
+  UINT Message,
+  WPARAM WParam,
+  LPARAM LParam)
+{
+  LRESULT Result = 0;
+  switch(Message)
+  {
+    case WM_SIZE:
+      {
+        OutputDebugStringA("WM_SIZE\n");
+      }
+      break;
+    case WM_DESTROY:
+      {
+        OutputDebugStringA("WM_DESTROY\n");
+      }
+      break;
+    case WM_CLOSE:
+      {
+        OutputDebugStringA("WM_CLOSE\n");
+      }
+      break;
+    case WM_ACTIVATEAPP:
+      {
+        OutputDebugStringA("WM_ACTIVATEAPP\n");
+      }
+      break;
+    default:
+      {
+        // OutputDebugStringA("default\n");
+        Result = DefWindowProc(Window, Message, WParam, LParam);
+      }
+      break;
+  }
+  return(Result);
+}
+
 int CALLBACK WinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  LPSTR lpCmdLine,
-  int nCmdShow
-) {
-  MessageBox(
-    0,
-    "Ceci est FaitmainHeros.",
-    "FaitmainHero",
-    MB_OK | MB_ICONINFORMATION);
+  HINSTANCE Instance,
+  HINSTANCE PrevInstance,
+  LPSTR CommandLine,
+  int ShowCode)
+{
+  // Création de la fenêtre principale
+  WNDCLASS WindowClass = {}; // initialisation par défaut
+  // On ne configure que les membres que l'on veut
+  WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+  // WindowClass.hIcon;
+  WindowClass.lpszClassName = (LPCWSTR)"FaitmainHerosWindowClass"; // nom pour retrouver la fenêtre
+
+  // Ouverture de la fenêtre
+  RegisterClass(&WindowClass);
+
   return(0);
 };
