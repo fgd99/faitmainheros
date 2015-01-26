e9b1ef32d4fd4a4e2fb7ce95418fca3bb24fa9d3
fgd99 <fabriceguydominguez@gmail.com>
2015-01-26 20:40:01 +0100

    Ouverture d'une fenêtre Windows et premiers pas
    Ouverture d'une fenêtre affichage d'un rectangle de la taille de la
    fenêtre et d'un point

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 mkdir build
 pushd build
-cl -Zi ..\code\win32_faitmain.cpp user32.lib
+cl -Zi ..\code\win32_faitmain.cpp user32.lib Gdi32.lib
 popd
--- code/win32_faitmain.cpp
+++ code/win32_faitmain.cpp
@@ -29,6 +29,24 @@ LRESULT CALLBACK MainWindowCallback(
         OutputDebugStringA("WM_ACTIVATEAPP\n");
       }
       break;
+    case WM_PAINT:
+      {
+        PAINTSTRUCT Paint;
+        HDC DeviceContext = BeginPaint(Window, &Paint);
+        int X = Paint.rcPaint.left;
+        int Y = Paint.rcPaint.top;
+        int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+        int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+        static DWORD Operation = WHITENESS;
+        PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+        if (Operation == WHITENESS)
+          Operation = BLACKNESS;
+        else
+          Operation = WHITENESS;
+        SetPixel(DeviceContext, 100, 100, RGB(255, 0, 255));
+        EndPaint(Window, &Paint);
+      }
+      break;
     default:
       {
         // OutputDebugStringA("default\n");
@@ -46,16 +64,48 @@ int CALLBACK WinMain(
   int ShowCode)
 {
   // Création de la fenêtre principale
-  WNDCLASS WindowClass = {}; // initialisation par défaut
+  WNDCLASSA WindowClass = {}; // initialisation par défaut, ANSI version de WNDCLASSA
   // On ne configure que les membres que l'on veut
-  WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
   WindowClass.lpfnWndProc = MainWindowCallback;
   WindowClass.hInstance = Instance;
   // WindowClass.hIcon;
-  WindowClass.lpszClassName = (LPCWSTR)"FaitmainHerosWindowClass"; // nom pour retrouver la fenêtre
+  WindowClass.lpszClassName = "FaitmainHerosWindowClass"; // nom pour retrouver la fenêtre
 
   // Ouverture de la fenêtre
-  RegisterClass(&WindowClass);
+  if (RegisterClassA(&WindowClass))
+  {
+    HWND WindowHandle = CreateWindowExA( // ANSI version de CreateWindowEx
+      0, // dwExStyle : options de la fenêtre
+      WindowClass.lpszClassName,
+      "FaitmainHeros",
+      WS_OVERLAPPEDWINDOW|WS_VISIBLE, //dwStyle : overlapped window, visible par défaut
+      CW_USEDEFAULT, // X
+      CW_USEDEFAULT, // Y
+      CW_USEDEFAULT, // nWidth
+      CW_USEDEFAULT, // nHeight
+      0, // hWndParent : 0 pour dire que c'est une fenêtre top
+      0, // hMenu : 0 pour dire pas de menu
+      Instance,
+      0 // Pas de passage de paramètres à la fenêtre
+    );
+    if (WindowHandle) {
+      MSG Message;
+      for (;;) {
+        BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+        if (MessageResult > 0) {
+          TranslateMessage(&Message);
+          DispatchMessage(&Message);
+        } else {
+
+        }
+      }
+    } else {
+      OutputDebugStringA("Error: CreateWindowEx\n");
+    }
+  } else {
+    OutputDebugStringA("Error: RegisterClass\n");
+  }
 
   return(0);
 };
