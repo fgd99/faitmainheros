1e87a0c07e325fab459f8ea9ab10dbdaadb74baa
fgd99 <fabriceguydominguez@gmail.com>
2015-03-09 17:06:23 +0100

    Création d'un header file pour le main win32
    Pour commencer à y mettre des déclarations propres

--- code/win32_faitmain.cpp
+++ code/win32_faitmain.cpp
@@ -33,40 +33,12 @@ typedef double real64;
 #include <Xinput.h> // Pour la gestion des entrées (manette...)
 #include <dsound.h> // Pour jouer du son avec DirectSound
 #include <stdio.h>
-
-// Struct qui représente un backbuffer qui nous permet de dessiner
-struct win32_offscreen_buffer {
-  BITMAPINFO Info;
-  void *Memory;
-  int Width;
-  int Height;
-  int BytesPerPixel;
-  int Pitch; // Pitch représente la taille d'une ligne en octets
-};
-
-// Struct qui représente un buffer pour jouer du son
-struct win32_sound_output
-{
-  int SamplesPerSecond;
-  int16 ToneVolume;
-  uint32 RunningSampleIndex;
-  int BytesPerSample;
-  int SecondaryBufferSize;
-  int LatencySampleCount;
-  real32 tSine;
-};
+#include "win32_faitmain.h"
 
 // variables globales pour le moment, on gèrera autrement plus tard
 global_variable bool32 GlobalRunning;
 global_variable win32_offscreen_buffer GlobalBackBuffer;
 global_variable LPDIRECTSOUNDBUFFER GlobalSecondaryBuffer;
-
-// Struct qui représente des dimensions
-struct win32_window_dimension
-{
-  int Width;
-  int Height;
-};
 
 // Permet de renvoyer les dimensions actuelles de la fenêtre
 internal win32_window_dimension
---
+++ code/win32_faitmain.h
@@ -0,0 +1,33 @@
+#if !defined(WIN32_HANDMADE_H)
+
+// Struct qui représente un backbuffer qui nous permet de dessiner
+struct win32_offscreen_buffer {
+  BITMAPINFO Info;
+  void *Memory;
+  int Width;
+  int Height;
+  int BytesPerPixel;
+  int Pitch; // Pitch représente la taille d'une ligne en octets
+};
+
+// Struct qui représente des dimensions
+struct win32_window_dimension
+{
+  int Width;
+  int Height;
+};
+
+// Struct qui représente un buffer pour jouer du son
+struct win32_sound_output
+{
+  int SamplesPerSecond;
+  int16 ToneVolume;
+  uint32 RunningSampleIndex;
+  int BytesPerSample;
+  int SecondaryBufferSize;
+  int LatencySampleCount;
+  real32 tSine;
+};
+
+#define WIN32_HANDMADE_H
+#endif
