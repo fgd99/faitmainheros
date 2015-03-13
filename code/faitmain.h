776d1425109e68b12947447f9d85a31342b7356f
fgd99 <fabriceguydominguez@gmail.com>
2015-03-13 16:27:58 +0100

    Création d'une macro Assert
    Et utilisation pour vérifier la taille de la mémoire allouée par le code
    plateforme

--- code/faitmain.h
+++ code/faitmain.h
@@ -3,6 +3,8 @@
 /*
   Macros utiles
 */
+#define Assert(Expression) \
+  if(!(Expression)) {*(int *)0 = 0;}
 #define Kilobytes(value) ((value)*1024)
 #define Megabytes(value) (Kilobytes(value)*1024)
 #define Gigabytes(value) (Megabytes(value)*1024)
