48048c92a33d711d7124dfbbfd02d3e5045505b9
fgd99 <fabriceguydominguez@gmail.com>
2015-03-24 16:54:19 +0100

    Activation de la compilation incrémentale
    Et génération d'un fichier map

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ set path=C:\Users\Administrateur\Documents\faitmainheros\misc;%path%
 
 IF NOT EXIST build mkdir build
 pushd build
-cl -MT -nologo -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4701 -DFAITMAIN_INTERNAL=1 -DFAITMAIN_LENT=1 -DFAITMAIN_WIN32=1 -FC -Zi ..\code\win32_faitmain.cpp /link -subsystem:windows,5.1 user32.lib Gdi32.lib
+cl -MT -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4701 -DFAITMAIN_INTERNAL=1 -DFAITMAIN_LENT=1 -DFAITMAIN_WIN32=1 -FC -Z7 -Fmwin32_faitmain.map ..\code\win32_faitmain.cpp /link -subsystem:windows,5.1 user32.lib Gdi32.lib
 popd
