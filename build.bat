a1bd466db531554c6138e78631b2b8203d095ed8
fgd99 <fabriceguydominguez@gmail.com>
2015-03-25 16:56:49 +0100

    Une directive de compilation pour dire de ne rien optimiser
    Ce qui est pratique pour voir ce qu'il se passe

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ set path=C:\Users\Administrateur\Documents\faitmainheros\misc;%path%
 
 IF NOT EXIST build mkdir build
 pushd build
-cl -MT -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4701 -wd4127 -DFAITMAIN_INTERNAL=1 -DFAITMAIN_LENT=1 -DFAITMAIN_WIN32=1 -FC -Z7 -Fmwin32_faitmain.map ..\code\win32_faitmain.cpp /link -opt:ref -subsystem:windows,5.1 user32.lib Gdi32.lib
+cl -MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4701 -wd4127 -DFAITMAIN_INTERNAL=1 -DFAITMAIN_LENT=1 -DFAITMAIN_WIN32=1 -FC -Z7 -Fmwin32_faitmain.map ..\code\win32_faitmain.cpp /link -opt:ref -subsystem:windows,5.1 user32.lib Gdi32.lib
 popd
